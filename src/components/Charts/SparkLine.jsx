259b4bc3aa5ad0d48e33a8edcc74eba20600d0d8
Swapnil Bhattacharya <swapnilb050203@gmail.com>
2025-03-07 23:24:59 +0530

    Create Revenue Section

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-    return (
-        <div>SparkLine</div>
-    )
-}
-export default SparkLine
+const SparkLine = ({ id, currentColor, color, type, height, width, data }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="xval"
+      yName="yval"
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        format: "${xval} : data ${yval}",
+        trackLineSettings: {
+          visible: true,
+          color: currentColor,
+        },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
+};
+export default SparkLine;
--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const Stacked = () => {
-    return (
-        <div>Stacked</div>
-    )
-}
-export default Stacked
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../assets/dummy";
+
+const Stacked = ({ width, height }) => {
+  return (
+    <ChartComponent
+      width={width}
+      height={height}
+      id="charts"
+      legendSettings={{ background: "white" }}
+        primaryXAxis={stackedPrimaryXAxis}
+        primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
+export default Stacked;
